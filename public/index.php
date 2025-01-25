dd6f22d32f5b00b536539e1b02107b2f8b83ba91
ZXCpikachu <misha3.09@mail.ru>
2025-01-25 15:04:20 +0300

    composer

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
-
+require_once __DIR__ . '/../vendor/autoload.php';
+use Pikachu\ExampleLocal\Lib\A;
 echo A::getValue();
---
+++ src/lib/A.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pikachu\ExampleLocal\Lib;
+
+class A
+{
+    public static function getValue(){
+        return "Hello from class A!";
+    }
+}
